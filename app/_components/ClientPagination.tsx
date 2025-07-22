6a45b6c3b9a755f225898cfdc1555437e39c9207
Reza Salehi <itssorenadev@gmail.com>
2025-07-22 16:34:21 +0330

    fix

--- app/_components/ClientPagination.tsx
+++ app/_components/ClientPagination.tsx
@@ -1,21 +1,32 @@
-import { Movie } from "./OptimizedMovieImg";
+// import { Movie } from "./OptimizedMovieImg";
 
-interface Props {
-    initialData: Movie[];
-    media: "movie" | "tv";
-    list: "popular" | "top_rated" | "now_playing";
-}
+// interface Props {
+//     initialData: Movie[];
+//     media: "movie" | "tv";
+//     list: "popular" | "top_rated" | "now_playing";
+// }
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-export default function ClientPagination({ initialData, media, list }: Props) {
-    const PAGE_SIZE = 10;
+// export default function ClientPagination({ initialData, media, list }: Props) {
+//     const PAGE_SIZE = 10;
 
-    const getKey = (pageIndex: number, previousPageData: Movie[] | null) => {
-        if (previousPageData && previousPageData.length < PAGE_SIZE) return null;
+//     const getKey = (pageIndex: number, previousPageData: Movie[] | null) => {
+//         if (previousPageData && previousPageData.length < PAGE_SIZE) return null;
 
-        return `/api/${media}/${list}?page=${pageIndex + 1}`;
-    }
+//         return `/api/${media}/${list}?page=${pageIndex + 1}`;
+//     }
 
-    return <div>ClientPagination</div>;
-}
+//     const {
+//         data,
+//         size,
+//         setSize,
+//         isValidating,
+//         error,
+//       } = useSWR<Movie[]>(getKey, fetcher, {
+//         initialData: [initialMovies],
+//         revalidateFirstPage: false,
+//       });
+
+//     return <div>ClientPagination</div>;
+// }
