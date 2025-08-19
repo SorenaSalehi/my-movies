7c88505594636a063467348a192c6a57e3d8ff11
Reza Salehi <itssorenadev@gmail.com>
2025-08-19 15:18:46 +0330

    testing theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,7 +63,6 @@ export default function RootLayout({
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="dark"
-                    enableSystem
                     disableTransitionOnChange
                 >
                     <ReactQueryProvider>
